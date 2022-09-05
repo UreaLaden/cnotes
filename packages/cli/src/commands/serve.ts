fd6a13000554564d0b5843ed0aa833a52db39a9e
Leaundrae Mckinney <pytechdrae@gmail.com>
2022-09-05 13:42:54 -0400

    Begin configuring Express Server for local-api and CLI interactions

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,6 +1,12 @@
 import path from 'path';
 import { Command } from 'commander';
 import {serve} from 'local-api';
+
+
+interface LocalApiError{
+    code:string;
+}
+
 /**[] -- Optional
  * <> -- Required
  * Documentation located at: https://www.npmjs.com/package/commander 
@@ -9,7 +15,24 @@ export const serveCommand = new Command()
 .command('serve [filename]')
 .description('Open a file for editing')
 .option('-p, --port <number>','port to run server on','4005')
-.action((filename = 'nodebook.js',options:{port:string}) => {
-    const dir = path.join(process.cwd(),path.dirname(filename));
-    serve(parseInt(options.port),path.basename(filename),dir);
+.action(async (filename = 'nodebook.js',options:{port:string}) => {
+    const isLocalApiError = (error:any):error is LocalApiError =>{
+        return typeof error.code === "string";
+    };
+    try{
+        const dir = path.join(process.cwd(),path.dirname(filename));
+        await serve(parseInt(options.port),path.basename(filename),dir);
+        console.log(`
+            Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.
+        `)
+    }catch(error){
+        if (isLocalApiError(error)){
+            if(error.code === "EADDRINUSE"){
+                console.error("Port is in use. Try running on a different port.");
+            }
+        }else if (error instanceof Error){
+            console.error('Heres the problem. ',error.message);
+        }
+        process.exit(1);
+    }
 });
